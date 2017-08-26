435e531e0bf3a34d372e56bfab3db902e698b9fa
chinafuqh <chinafuqh@hotmail.com>
2017-08-26 12:22:37 +0800

    Add files via upload

---
+++ godOfGame.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class godOfGame : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public IEnumerator playerDead()
+    {
+
+        GameObject.FindGameObjectWithTag("MainCamera").GetComponent<CameraFollow>().enabled = false;
+        yield return new WaitForSeconds(2f);
+        Destroy(GameObject.FindGameObjectWithTag("Player"));
+        // ... reload the level.
+        StartCoroutine("ReloadGame");
+
+
+    }
+
+    IEnumerator ReloadGame()
+    {
+        // ... pause briefly
+        yield return new WaitForSeconds(2);
+        // ... and then reload the level.
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex, LoadSceneMode.Single);
+    }
+
+
+}
---
+++ joystick.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+
+/// <summary>  
+/// 测试游戏手柄键值  
+/// </summary>  
+public class joystick : MonoBehaviour
+{
+    private string currentButton;//当前按下的按键  
+
+    // Use this for initialization   
+    void Start()
+    {
+
+    }
+    // Update is called once per frame   
+    void Update()
+    {
+        var values = Enum.GetValues(typeof(KeyCode));//存储所有的按键  
+        for (int x = 0; x < values.Length; x++)
+        {
+            if (Input.GetKeyDown((KeyCode)values.GetValue(x)))
+            {
+                currentButton = values.GetValue(x).ToString();//遍历并获取当前按下的按键  
+            }
+        }
+    }
+    // Show some data   
+    void OnGUI()
+    {
+        GUI.TextArea(new Rect(0, 0, 250, 40), "Current Button : " + currentButton);//使用GUI在屏幕上面实时打印当前按下的按键  
+    }
+}
